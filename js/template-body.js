b1a9b20753d86ae54665cac3f0c89435dcc0d4b8
Lidia A <90976607+LidiaA3@users.noreply.github.com>
2022-04-18 13:46:11 +0200

    Add files via upload

---
+++ js/template-body.js
@@ -0,0 +1,42 @@
+document.write('\
+      <header class="main-header">\
+          <nav id="nav" class="main-nav">\
+            <div class="nav-links">\
+              <a class="link-item" href="#">Diseñador 1</a>\
+              <a class="link-item" href="#">Diseñador 2</a>\
+              <a class="link-item" href="#">Diseñador 3</a>\
+              <a class="link-item" href="#">Diseñador 4</a>\
+            </div>\
+          </nav>\
+          <button id="button-menu" class="button-menu">\
+            <span></span>\
+            <span></span>\
+            <span></span>\
+          </button>\
+      </header>\
+\
+      <main>\
+        <div class="container">\
+          <!--img width="100%" src="imgs/ref-poster.jpg" alt="ñññ"-->\
+          <video id="srcPoster" controls autoplay muted poster="imgs/ref-poster.jpg" width="100%" height="100%">\
+            <source id="srcVideo" src="imgs/#.mp4" type="video/mp4">\
+              Tu navegador no soporta este video.\
+          </video>\
+        </div>\
+        <div class="cuadro-texto">\
+          <h2 id="titulo" class="titulo">Título obra</h2>\
+          <p id="cuerpo" class="cuerpo">Texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción texto descripción.</p>\
+          <p id="autor" class="autor">Nombre autor</p>\
+        </div>\
+      </main>\
+\
+      <footer>\
+        <hr class="divisor">\
+        <div class="logos-footer">\
+          <img src="imgs/logo1.png" alt="logo1">\
+          <img src="imgs/logo2.png" alt="logo2">\
+          <img src="imgs/logo3.png" alt="logo3">\
+        </div>\
+        <p class="legal">&copy Derechos reservados</p>\
+      </footer>\
+');
---
+++ js/template-head.js
@@ -0,0 +1,8 @@
+document.write('\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <link rel="shortcut icon" href="imgs/icon-favicon.svg">\
+    <link rel="stylesheet" href="css/style.css">\
+    <link rel="stylesheet" href="css/menu.css">\
+    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>\
+    <script src="js/menu.js"></script>\
+');
